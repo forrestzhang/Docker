533eaea92cd6644cc4c04c09f565f778fbaa72ef
Tao Zhang <forrestzhang1982@gmail.com>
2016-04-27 15:33:41 -0500

    add user support

---
+++ meme/startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo $CURRENT_UID $CURRENT_USER
+
+
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if ! id -u $CURRENT_USER > /dev/null 2>&1; then
+  echo "use local user: " ${BLUE}$CURRENT_USER${NC}
+  adduser -u  $CURRENT_UID $CURRENT_USER
+fi
+
+
+if [ -e $CURRENT_DATA ];
+  then
+    echo ${GREEN}$CURRENT_DATA${NC} exists
+    chown -R $CURRENT_UID $CURRENT_DATA
+else
+  mkdir -p $CURRENT_DATA
+  chown -R $CURRENT_UID $CURRENT_DATA
+fi
+
+
+for arg in "$*"
+do
+  	exec  $*
+done
